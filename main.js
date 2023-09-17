6eeca1d59c13c90f72b859efbf2c622bcc2fb4b2
wwwhin <124798355+wwwhin@users.noreply.github.com>
2023-09-17 01:55:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var enter = document.getElementById('enter');
+enter.addEventListener('click', function(){
+    window.open('content.html')
+})
+
+ function open2(){
+    window.open('source.html')
+}
+
+function open1(){
+    window.open('https://www.youtube.com/channel/UCv08TBrCr6mha3apzMKrD5A')
+}
